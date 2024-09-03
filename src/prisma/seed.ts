c62c96b3b3c9ffa230271de169114df58490db33
War-helsink <cnhfntu1000@gmail.com>
2024-09-03 16:12:13 +0300

    update

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -10,28 +10,8 @@ import {
 	productItemPrices,
 } from "./constants";
 import { prisma } from "./prisma-client";
-import { hashSync } from "bcrypt";
 
 async function up() {
-	await prisma.user.createMany({
-		data: [
-			{
-				fullName: "User Test",
-				email: "user@test.ru",
-				password: hashSync("111111", 10),
-				verified: new Date(),
-				role: "USER",
-			},
-			{
-				fullName: "Admin Admin",
-				email: "admin@test.ru",
-				password: hashSync("111111", 10),
-				verified: new Date(),
-				role: "ADMIN",
-			},
-		],
-	});
-
 	await prisma.category.createMany({
 		data: categories,
 	});
