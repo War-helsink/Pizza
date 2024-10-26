6d604fc94ac655c7d4df7f3d7622e7d3181b6782
War-helsink <cnhfntu1000@gmail.com>
2024-10-26 19:32:34 +0300

    update

--- src/components/features/sort/ui/SortPopup/index.tsx
+++ src/components/features/sort/ui/SortPopup/index.tsx
@@ -2,17 +2,40 @@
 
 import { cn } from "@/libs/utils";
 import { ArrowUpDown } from "lucide-react";
-import { useTranslation } from "react-i18next";
 import {
 	Popover,
+	PopoverClose,
 	PopoverContent,
 	PopoverTrigger,
 } from "@/components/shared/ui";
 
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+
 import type { SortPopupProps } from "../../model/props";
 
 export const SortPopup: React.FC<SortPopupProps> = ({ className }) => {
+	const [sort, setSort] = useState("popular");
 	const { t } = useTranslation();
+
+	const sorts = [
+		{
+			label: "sort.sortByPopular",
+			value: "popular",
+		},
+		{
+			label: "sort.sortByCheap",
+			value: "sortByCheap",
+		},
+		{
+			label: "sort.sortByExpensive",
+			value: "sortByExpensive",
+		},
+		{
+			label: "sort.sortByRating",
+			value: "sortByRating",
+		},
+	];
 
 	return (
 		<Popover>
@@ -26,23 +49,29 @@ export const SortPopup: React.FC<SortPopupProps> = ({ className }) => {
 					<ArrowUpDown className="w-4 h-4" />
 					<b>{t("sort.label")}</b>
 
-					<b className="text-primary">{t("sort.popular")}</b>
+					<b className="text-primary">{t(`sort.${sort}`)}</b>
 				</div>
 			</PopoverTrigger>
 			<PopoverContent className="w-[240px]">
-				<ul className="*:rounded-md *:cursor-pointer *:px-4">
-					<li className="hover:bg-secondary hover:text-primary p-2">
-						{t("sort.sortByPopular")}
-					</li>
-					<li className="hover:bg-secondary hover:text-primary p-2">
-						{t("sort.sortByCheap")}
-					</li>
-					<li className="hover:bg-secondary hover:text-primary p-2">
-						{t("sort.sortByExpensive")}
-					</li>
-					<li className="hover:bg-secondary hover:text-primary p-2">
-						{t("sort.sortByRating")}
-					</li>
+				<ul>
+					{sorts.map((item) => (
+						<li
+							key={item.value}
+							className={cn(
+								"hover:bg-secondary hover:text-primary rounded-md cursor-pointer ",
+								{
+									"bg-secondary text-primary": item.value === sort,
+								},
+							)}
+						>
+							<PopoverClose
+								onClick={() => setSort(item.value)}
+								className="w-full h-full px-4 py-2 focus-visible:outline-none"
+							>
+								{t(item.label)}
+							</PopoverClose>
+						</li>
+					))}
 				</ul>
 			</PopoverContent>
 		</Popover>
